73b0dc977d386000dfaabe12efeeebae8e0faf46
Sergiu Ghenciu <1024gs@gmail.com>
2024-11-17 15:41:41 +0000

    Install instanbul

--- scripts/pipe-sass.js
+++ scripts/pipe-sass.js
@@ -3,7 +3,8 @@ const sass = require("node-sass");
 const pipe = (options) => ({ fileName, content }) => {
   return new Promise((resolve) => {
     const result = sass.renderSync({ data: content, file: fileName });
-
+    // const result = sass.compile()
+    // const result = sass.compileString()
     resolve({ fileName, content: result.css });
   });
 };
@@ -11,3 +12,13 @@ const pipe = (options) => ({ fileName, content }) => {
 module.exports = {
   sass: pipe,
 };
+
+
+// const sass = require('sass');
+
+// const result = sass.compile(scssFilename);
+
+// // OR
+
+// // Note that `compileAsync()` is substantially slower than `compile()`.
+// const result = await sass.compileAsync(scssFilename);
