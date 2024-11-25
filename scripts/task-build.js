eca45d50c7f10d496dcb7c5ea2d9ce4663ccdd04
Sergiu Ghenciu <1024gs@gmail.com>
2024-11-25 17:30:12 +0000

    Rename the tasks files

--- scripts/task-build.js
+++ scripts/task-build.js
@@ -1,7 +1,7 @@
-const { bundle } = require("./mod-bundle.js");
-const { concatenate } = require("./mod-concatenate.js");
-const { minifyCss, minifyJs } = require("./mod-minify.js");
-const { removeDevCode } = require("./mod-remove-dev-code.js");
+const { bundle } = require("./tasks-bundle.js");
+const { concatenate } = require("./tasks-concatenate.js");
+const { minifyCss, minifyJs } = require("./tasks-minify.js");
+const { removeDevCode } = require("./tasks-remove-dev-code.js");
 
 const build = async () => {
   await bundle({ isProduction: true });
